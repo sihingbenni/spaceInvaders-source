bd9319aa53a7eefebaa0cc6168ca4372a5f89de2
Benjamin Puhani <bpuhani@usfca.edu>
2025-02-27 13:35:47 -0800

    MOD: do not use config, but env files

--- models/index.js
+++ models/index.js
@@ -1,45 +1,45 @@
 'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const db = {};
+let sequelize;
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env.DB_URL, {
-    dialect: 'mysql',
-    protocol: 'mysql',
-    logging: true
-  });
+if (env !== 'development') {
+    sequelize = new Sequelize(process.env.DB_URL, {
+        dialect: 'mysql',
+        protocol: 'mysql',
+        logging: true
+    });
 } else {
-  var sequelize = new Sequelize(
-      process.env.DB_NAME || config.database,
-      process.env.DB_USER || config.username,
-      process.env.DB_PASS || config.password,
-      {
-        host: process.env.DB_HOST || config.host,
-        dialect: config.dialect
-      }
-  );
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASS,
+        {
+            host: process.env.DB_HOST,
+            dialect: 'mysql'
+        }
+    );
 }
 
 fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+        const model = sequelize['import'](path.join(__dirname, file));
+        db[model.name] = model;
+    });
 
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 db.sequelize = sequelize;
