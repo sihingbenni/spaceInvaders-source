453a7ae166465045e6eb0d4ae8b9df9be83f5347
Benjamin Puhani <bpuhani@usfca.edu>
2025-03-02 15:51:31 -0800

    FIX: try to use http

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const http = require('http');
 const cheerio = require('cheerio');
 const expect = require('chai').expect;
 
@@ -14,38 +14,87 @@ describe("environements", () => {
 describe("spaceInvaders", function() {
   this.timeout(30000);
 
-  it("should send user to the game page", async function() {
-    const response = await axios.get(process.env.FRONTEND_URL);
-    const $ = cheerio.load(response.data);
-    const gameLink = $("a[href='/game']");
-    expect(gameLink).to.not.equal(undefined);
+  it("should send user to the game page", function(done) {
+    http.get(process.env.FRONTEND_URL, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        const $ = cheerio.load(data);
+        const gameLink = $("a[href='/game']");
+        expect(gameLink).to.not.equal(undefined);
+        done();
+      });
+    }).on('error', (err) => {
+      done(err);
+    });
   });
 
-  it("should send user to the leaderboard page", async function() {
-    const response = await axios.get(process.env.FRONTEND_URL);
-    const $ = cheerio.load(response.data);
-    const leaderboardLink = $("a[href='/leaderboard']");
-    expect(leaderboardLink).to.not.equal(undefined);
+  it("should send user to the leaderboard page", function(done) {
+    http.get(process.env.FRONTEND_URL, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        const $ = cheerio.load(data);
+        const leaderboardLink = $("a[href='/leaderboard']");
+        expect(leaderboardLink).to.not.equal(undefined);
+        done();
+      });
+    }).on('error', (err) => {
+      done(err);
+    });
   });
 
-  it("should create a new record", async function() {
-    const response = await axios.post(process.env.BACKEND_URL + 'api/score', {
+  it("should create a new record", function(done) {
+    const postData = JSON.stringify({
       user_name: 'test',
       score: -100
     });
-    expect(response.status).to.equal(200);
+
+    const options = {
+      hostname: 'localhost',
+      port: 8080,
+      path: '/api/score',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(postData)
+      }
+    };
+
+    const req = http.request(options, (res) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+
+    req.on('error', (err) => {
+      done(err);
+    });
+
+    req.write(postData);
+    req.end();
   });
 
-  it("should show the record on the leaderboard", async function() {
-    const response = await axios.get(process.env.FRONTEND_URL + 'leaderboard', {
-      timeout: 5000, // Increase timeout to 5 seconds
-      proxy: false
+  it("should show the record on the leaderboard", function(done) {
+    http.get(process.env.FRONTEND_URL + 'leaderboard', (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        const $ = cheerio.load(data);
+        const record = $("tr").last().children();
+        console.log($('tr').last().html());
+        expect(record.eq(0).text()).to.equal('test');
+        expect(record.eq(1).text()).to.equal('-100');
+        done();
+      });
+    }).on('error', (err) => {
+      done(err);
     });
-    const $ = cheerio.load(response.data);
-    const record = $("tr").last().children();
-    console.log($('tr').last().html());
-    expect(record.eq(0).text()).to.equal('test');
-    expect(record.eq(1).text()).to.equal('-100');
   });
 
 });
